5ede762de52e54ee0f69bbe0e47dcc5e884341a6
Samuel Tanner <samtann10@gmail.com>
2026-03-25 23:54:55 +0000

    finish email events

--- models/ecoessentials/e_dim_time.sql
+++ models/ecoessentials/e_dim_time.sql
@@ -1,37 +1,18 @@
 {{ config(
-   materialized = 'table',
-   schema = 'dw_eco_essentials'
-)}}
+    materialized='table',
+    schema='dw_eco_essentials'
+) }}
 
-
--- with cte_time as (
---     select 
---         seq4() % 24 as hour,
---         (seq4() / 24) % 60 as minute,
---         (seq4() / (24*60)) % 60 as second
---     from table(generator(rowcount => 86400))
--- )
-
--- select
---     {{ dbt_utils.generate_surrogate_key(['hour', 'minute', 'second']) }} as time_key,
---     hour,
---     minute,
---     second,
---     CONCAT(hour, ':', minute, ':', second) as time_string
--- from cte_time
-
-with cte as (
-    select 
-        extract(hour from eventtimestamp) as hour,
-        extract(minute from eventtimestamp) as minute,
-        extract(second from eventtimestamp) as second
-    from {{ source('eco_emails_landing', 'MARKETINGEMAILS') }}
+WITH time_data AS (
+    SELECT DISTINCT
+        eventtimestamp
+    FROM {{ source('eco_emails_landing', 'MARKETINGEMAILS') }}
 )
 
-select
-    {{ dbt_utils.generate_surrogate_key(['hour', 'minute', 'second']) }} as time_key,
-    hour,
-    minute,
-    second,
-    CONCAT(hour, ':', minute, ':', second) as time_string
-from cte
+SELECT
+    TO_CHAR(eventtimestamp, 'HH24MISS') AS time_key,
+    DATE_PART(hour, eventtimestamp) AS hour,
+    DATE_PART(minute, eventtimestamp) AS minute,
+    DATE_PART(second, eventtimestamp) AS second,
+    TO_CHAR(eventtimestamp, 'HH24:MI:SS') AS time_string
+FROM time_data
--- models/ecoessentials/e_fact_EmailEvent.sql
+++ models/ecoessentials/e_fact_EmailEvent.sql
@@ -3,13 +3,25 @@
 SELECT 
     t.time_key,
     d.date_key,
-    cu.customer_key.
+    cu.customer_key,
     c.campaign_key,
     e.event_key,
     em.emailid_key
-FROM {{ source('eco_emails_landing', 'MARKETINGEMAILS') }} e
+FROM {{ source('eco_emails_landing', 'MARKETINGEMAILS') }} me
 JOIN {{ ref('e_dim_date') }} d 
-JOIN {{ ref('e_dim_customer') }} cu
+    ON d.date = CAST(me.eventtimestamp AS DATE)
+JOIN {{ ref('e_dim_time') }} t
+    ON t.time_key = TO_CHAR(me.eventtimestamp, 'HH24MISS')
+-- JOIN {{ ref('e_dim_customer') }} cu 
+--     ON cu.customer_id = me.customerid
+--could be duplicates, but there are missing customerids in the source table
+JOIN {{ ref('e_dim_customer') }} cu 
+    ON cu.customer_first_name = me.subscriberfirstname
+   AND cu.customer_last_name = me.subscriberlastname
+   AND cu.customer_email = me.subscriberemail
 JOIN {{ ref('e_dim_campaign') }} c 
-JOIN {{ ref('e_dim_event')}} e
-JOIN {{ ref('e_emailid')}} em
+    ON c.campaign_id = me.campaignid
+JOIN {{ ref('e_dim_event') }} e 
+    ON e.emaileventid = me.emaileventid
+JOIN {{ ref('e_dim_emailid') }} em 
+    ON em.emailid = me.emailid
