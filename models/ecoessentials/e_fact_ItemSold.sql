480dd3382a04e1df54cd7c3f03fc9e292e241524
Samuel Tanner <samtann10@gmail.com>
2026-03-25 20:18:23 +0000

    fact_itemsold populated

--- models/ecoessentials/e_fact_ItemSold.sql
+++ models/ecoessentials/e_fact_ItemSold.sql
@@ -1,20 +1,43 @@
-{{ config( materialized = 'table', schema = 'dw_eco_essentials' ) }} 
+-- {{ config( materialized = 'table', schema = 'dw_eco_essentials' ) }} 
 
+-- SELECT 
+--     d.date_key, 
+--     cu.customer_key,
+--     p.product_key, 
+--     c.campaign_key, 
+--     ps.price, 
+--     ol.quantity, 
+--     ol.discount, 
+--     o.order_id 
+-- FROM {{source('eco_essentials_landing', 'orders')}} o
+-- INNER JOIN {{source('eco_essentials_landing', 'order_line')}} ol ON o.order_id = ol.order_id
+-- INNER JOIN {{source('eco_essentials_landing', 'product')}} ps ON ps.product_id = ol.product_id
+-- INNER JOIN {{ref('e_dim_customer')}} cu ON cu.customer_id = o.customer_id
+-- INNER JOIN {{ref('e_dim_product')}} p ON p.product_id = ol.product_id
+-- INNER JOIN {{ref('e_dim_campaign')}} c ON c.campaign_id = ol.campaign_id
+-- INNER JOIN {{ ref('dim_date') }} d ON d.date = CAST(o.order_timestamp AS DATE)
 
-SELECT
-   o.order_id,
-   p.product_key,
-   c.campaign_key,
-   cu.customer_key,
-   d.date_key,
-   ps.price,
-   ol.quantity,
-   ol.discount,
-   ol.price_after_discount
-FROM {{source('eco_essentials_landing', 'orders')}} o
-INNER JOIN {{source('eco_essentials_landing', 'ORDER_LINE')}} ol ON o.order_id = ol.order_id
-INNER JOIN {{source('eco_essentials_landing', 'product')}} ps ON ps.product_id = ol.product_id
-INNER JOIN {{ref('e_dim_customer')}} cu ON cu.customer_id = o.customer_id
-INNER JOIN {{ref('e_dim_product')}} p ON p.product_id = ol.product_id
-INNER JOIN {{ref('e_dim_campaign')}} c ON c.campaign_id = ol.campaign_id
-INNER JOIN {{ref('e_dim_date')}} d ON d.date = CAST(o.order_timestamp AS DATE)
+{{ config(materialized='table', schema='dw_eco_essentials') }}
+
+SELECT 
+    d.date_key, 
+    cu.customer_key,
+    p.product_key, 
+    c.campaign_key, 
+    ps.price, 
+    ol.quantity, 
+    ol.discount, 
+    o.order_id 
+FROM {{ source('eco_essentials_landing', 'orders') }} o
+JOIN {{ source('eco_essentials_landing', 'order_line') }} ol 
+    ON o.order_id = ol.order_id
+JOIN {{ source('eco_essentials_landing', 'product') }} ps 
+    ON ps.product_id = ol.product_id
+JOIN {{ ref('e_dim_customer') }} cu 
+    ON cu.customer_id = o.customer_id
+JOIN {{ ref('e_dim_product') }} p 
+    ON p.product_id = ol.product_id
+JOIN {{ ref('e_dim_campaign') }} c 
+    ON c.campaign_id = ol.campaign_id
+JOIN {{ ref('e_dim_date') }} d 
+    ON d.date = DATE(o.order_timestamp)
